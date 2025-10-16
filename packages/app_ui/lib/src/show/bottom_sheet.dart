191e936ff5773203f1392ec1be5967ff7ac91408
Duu1at <dbolsunbekuulu@gmail.com>
2025-10-17 00:50:31 +0600

    add new modal bottom sheet show

--- packages/app_ui/lib/src/show/bottom_sheet.dart
+++ packages/app_ui/lib/src/show/bottom_sheet.dart
@@ -1,5 +1,7 @@
 import 'package:app_ui/src/colors/app_theme_color_extension.dart';
 import 'package:app_ui/src/spacing/app_spacing.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 abstract class BottomSheets {
@@ -34,5 +36,92 @@ abstract class BottomSheets {
         ),
       ),
     );
+  }
+
+  static Future<T?> show<T>(
+    BuildContext context, {
+    List<Widget>? content,
+    List<CustomActionButton>? actions,
+    Widget? header,
+    String? title,
+    bool useRootNavigator = true,
+    Color? backgroundColor,
+  }) async {
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.iOS:
+      case TargetPlatform.macOS:
+        return showCupertinoModalPopup<T>(
+          context: context,
+          useRootNavigator: useRootNavigator,
+          builder: (BuildContext context) => CupertinoActionSheet(
+            actions: actions,
+            title: header != null || title != null
+                ? Column(
+                    children: [
+                      if (header != null) header,
+                      if (title != null)
+                        Text(
+                          title,
+                          style: AppFonts.font.mSemibold.copyWith(
+                            color: Theme.of(
+                              context,
+                            ).extension<CustomMainThemeData>()?.secondary,
+                          ),
+                        ),
+                    ],
+                  )
+                : null,
+            message: content != null ? Column(children: content) : null,
+            cancelButton: CupertinoActionSheetAction(
+              onPressed: () {
+                GoRouter.of(context).pop();
+              },
+              child: Text(
+                CoreLocaleKeys.buttons_cancel_title.trc(),
+                style: AppFonts.font.mSemibold.copyWith(
+                  color: Theme.of(
+                    context,
+                  ).extension<CustomButtonThemeData>()?.primary?.defaultBckgr,
+                ),
+              ),
+            ),
+          ),
+        );
+      case TargetPlatform.android:
+      case TargetPlatform.fuchsia:
+      case TargetPlatform.linux:
+      case TargetPlatform.windows:
+        return showModalBottomSheet<T>(
+          context: context,
+          showDragHandle: true,
+          useRootNavigator: useRootNavigator,
+          backgroundColor: backgroundColor,
+          builder: (BuildContext context) {
+            return Padding(
+              padding: const EdgeInsets.only(
+                bottom: AppSpacing.s8x,
+                top: AppSpacing.s2x,
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  if (header != null) header,
+                  if (title != null)
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: AppSpacing.s4x,
+                        bottom: AppSpacing.s2x,
+                      ),
+                      child: CustomTitle.h3(title),
+                    ),
+                  ...actions as List<Widget>,
+                ],
+              ),
+            );
+          },
+        );
+    }
   }
 }
